7f4d69925aa64c5ab9b459be4a019f331ed48485
Inna Poliakova <polyakova1081@gmail.com>
2023-02-11 16:26:19 +0000

    Bugs fixed

--- script.js
+++ script.js
@@ -2,22 +2,38 @@
 
 const url = "https://api.tvmaze.com/shows/82/episodes";
 let allEpisodes = [];
+async function setup() {
+ try {
+     const response = await fetch(url);
+   const data = await response.json();
+  const allEpisodes = data;
+   makePageForEpisodes(allEpisodes);
+ } catch (error) {
+  console.error(error);
+ }
+ }
 
-//You can edit ALL of the code here
-function setup() {
-  fetch(url)
-    .then((res) => res.json())
-    .then((data) => {
-      // in here we can do whatever we want with the data
-      allEpisodes = data;
-      makePageForEpisodes(allEpisodes);
-    })
-    .catch((err) => console.error(err));
-}
+//  one more way:
+// const url = "https://api.tvmaze.com/shows/82/episodes";
+// let allEpisodes = [];
+// function setup() {
+//   fetch(url)
+//     .then((res) => res.json())
+//     .then((data) => {
+//       // in here we can do whatever we want with the data
+//       allEpisodes = data;
+//       makePageForEpisodes(allEpisodes);
+//     })
+//     .catch((err) => console.error(err));
+// }
+
+// without API:
 // function setup() {
 //   const allEpisodes = getAllEpisodes();
 //   makePageForEpisodes(allEpisodes);
 // }
+
+
 
 function makeSeasonAndEpisode(episode) {
   const { season, number } = episode;
@@ -47,26 +63,30 @@ defaultOption.text = "Select an episode";
 defaultOption.value = "";
 select.add(defaultOption);
 
+
 for (const episode of allEpisodes) {
   const option = document.createElement("option");
   option.text = `S${episode.season
       .toString()
       .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")} - ${episode.name} `;
-  option.value = `S0${episode.season}E0${episode.episode}`;
+  option.value = `S0${episode.season}E0${episode.number}`;
   select.add(option);
 }
+ 
 
 
 select.addEventListener("change", function() {
+  
   const selectedEpisode = this.value
   episodeContainer.innerHTML = "";
 
   for (const episode of allEpisodes) {
-    if (`S0${episode.season}E0${episode.episode}` === selectedEpisode) {
+    if (`S0${episode.season}E0${episode.number}` === selectedEpisode) {
       
       const episodeElement = document.createElement("div");
       episodeElement.classList.add("episode-filter");
-      episodeElement.innerHTML = `
+      episodeElement.innerHTML = 
+      `
         <h2>${`${episode.name} - S${episode.season
           .toString()
           .padStart(2, "0")}E${episode.number.toString().padStart(2, "0")}`}</h2>
@@ -80,6 +100,7 @@ select.addEventListener("change", function() {
     }
   }
 });
+
 
 
 // create a new input element and assigns it to the searchInput constant:
@@ -174,6 +195,8 @@ searchInput.addEventListener("input", function() {
   // calls the renderEpisodes function to re-render the episodes on the page, using only the filtered episodes
 });
 
+
+
 // function which responsible for rendering the list of episodes on the page:
 function renderEpisodes(episodes) {
  
@@ -205,83 +228,4 @@ function renderEpisodes(episodes) {
 window.onload = setup;
 
 
-// const url = "https://api.tvmaze.com/shows/82/episodes";
-// let allEpisodes = [];
-
-// //You can edit ALL of the code here
-// function setup() {
-//   fetch(url)
-//     .then((res) => res.json())
-//     .then((data) => {
-//       // in here we can do whatever we want with the data
-//       allEpisodes = data;
-//       makePageForEpisodes(allEpisodes);
-//     })
-//     .catch((err) => console.error(err));
-// }
-
-// function makeSeasonAndEpisode(episode) {
-//   const { season, number } = episode;
-//   const paddedSeason = season.toString().padStart(2, "0");
-//   const paddedEpisode = number.toString().padStart(2, "0");
-
-//   return `S${paddedSeason}E${paddedEpisode}`;
-// }
-
-// function makePageForEpisodes(episodeList) {
-//   const rootElem = document.getElementById("root");
-//   const selectElem = document.getElementById("select-input");
-
-//   // clear out the rootElement's HTML before we add the new stuff
-//   rootElem.innerHTML = "";
-//   const countParagraph = document.createElement("p");
-//   countParagraph.innerText = `Showing ${episodeList.length} episodes`;
-//   rootElem.appendChild(countParagraph);
-
-//   episodeList.forEach((episode) => {
-//     // add the season and episode and name
-//     const paragraph = document.createElement("p");
-//     paragraph.textContent = `${makeSeasonAndEpisode(episode)}: ${episode.name}`;
-//     rootElem.appendChild(paragraph);
-
-//     // add the image
-//     const image = document.createElement("img");
-//     image.src = episode.image.medium;
-//     rootElem.appendChild(image);
-
-//     // add the summary paragraph nb the episode.summary is actually HTML
-//     rootElem.innerHTML += episode.summary;
-
-//     // also, one more thing, add it to the select element as an option
-//     const option = document.createElement("option");
-//     option.textContent = `${makeSeasonAndEpisode(episode)} - ${episode.name}`;
-//     option.value = episode.id;
-//     selectElem.appendChild(option);
-//   });
-// }
-
-// const searchInput = document.getElementById("search-input");
-// searchInput.addEventListener("input", (e) => {
-//   const searchString = e.target.value.toLowerCase();
-//   const filteredEpisodes = allEpisodes.filter((episode) => {
-//     // localeCompare might be neater here
-//     return (
-//       episode.summary.toLowerCase().includes(searchString) ||
-//       episode.name.toLowerCase().includes(searchString)
-//     );
-//   });
-//   makePageForEpisodes(filteredEpisodes);
-// });
-
-// const selectInput = document.getElementById("select-input");
-// selectInput.addEventListener("change", (e) => {
-//   // we now have shown that e.target.value === the episode id that has been clicked on
-//   const idSelectedByUser = Number(e.target.value);
-//   const selectedEpisode = allEpisodes.find((ep) => ep.id === idSelectedByUser);
-//   if (selectedEpisode) {
-//     makePageForEpisodes([selectedEpisode]);
-//   }
-// });
-
-// window.onload = setup;
 
